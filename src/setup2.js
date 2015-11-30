de8a3f76b5bd4f0c9d13e1788c9be2c43b91d799
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2015-11-30 13:11:03 +0100

    update src
    finished setup2 src function

--- src/setup2.js
+++ src/setup2.js
@@ -1,11 +1,10 @@
 module.exports = function(n) {
-  if (typeof n === 'number') {
-    return n
-  }
-  if (typeof n === 'string') {
-    return n
-  }
-  if (typeof n === 'boolean') {
+  if (n.constructor === Array) {
+    if(n.length === 0)
+      return n
+    for(var i=0; i<n.length; i+=1){
+      n[i] = Math.round(((1/2)* (n[i].y - n[i].x) * (n[i].z - n[i].x))*10000)/10000
+    }
     return n
   }
   throw new Error('Does only accept scalar input')
