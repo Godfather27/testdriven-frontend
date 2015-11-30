ef230ea4e02febd4dec6cd6f40834bd868d85f08
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2015-11-30 01:34:16 +0100

    added tests
    added fixture test
    added scalar test
    added setup test
    added teardown test

---
+++ test/fixture.js
@@ -0,0 +1,20 @@
+var should = require('should')
+var fixture = require('../src/fixture.js')
+
+describe('fixture', function() {
+	it('first account should contain key "80ktk3yuui"', function() {
+        should(fixture.Accounts[0].key).equal('80ktk3yuui')
+    })
+
+    it('second account should contain key "e8ywfh7jmz"', function() {
+        should(fixture.Accounts[1].key).equal('e8ywfh7jmz')
+    })
+
+    it('first account should contain balance 24.25', function() {
+        should(fixture.Accounts[0].balance).equal(24.25)
+    })
+
+    it('second account should contain balance 0.00', function() {
+        should(fixture.Accounts[1].balance).equal(0.00)
+    })
+})
---
+++ test/setup.js
@@ -0,0 +1,30 @@
+var should = require('should')
+var setup = require('../src/setup.js')
+
+describe('setup', function() {
+    var testString
+
+    beforeEach(function() {
+        testString = "Lorem Ipsum dolor sit amet"
+    })
+
+    it('should return lowercase string', function() {
+        const result = setup(testString, "lower")
+        should(result).be.exactly("lorem ipsum dolor sit amet").and.be.a.String()
+    })
+
+    it('should return uppercase string', function() {
+    	const result = setup(testString, "upper")
+        should(result).be.exactly("LOREM IPSUM DOLOR SIT AMET").and.be.a.String()
+    })
+
+    it('should return capitalized string', function() {
+        const result = setup(testString, "capitalized")
+        should(result).be.exactly("Lorem ipsum dolor sit amet").and.be.a.String()
+     })
+
+    it('should return reversed string', function() {
+    	const result = setup(testString, "reversed")
+        should(result).be.exactly("tema tis rolod muspI meroL").and.be.a.String()
+    })
+})
