dad314b0884db69fd48661890f32df11a7b88af8
Sebastian Huber <shuber.mmt-b2014@fh-salzburg.ac.at>
2015-11-30 02:14:42 +0100

    refactor tests
    changed tabs to spaces

--- test/fixture.js
+++ test/fixture.js
@@ -3,18 +3,18 @@ var fixture = require('../src/fixture.js')
 
 describe('fixture', function() {
 	it('first account should contain key "80ktk3yuui"', function() {
-        should(fixture.Accounts[0].key).equal('80ktk3yuui')
-    })
+    should(fixture.Accounts[0].key).equal('80ktk3yuui')
+  })
 
-    it('second account should contain key "e8ywfh7jmz"', function() {
-        should(fixture.Accounts[1].key).equal('e8ywfh7jmz')
-    })
+  it('second account should contain key "e8ywfh7jmz"', function() {
+    should(fixture.Accounts[1].key).equal('e8ywfh7jmz')
+  })
 
-    it('first account should contain balance 24.25', function() {
-        should(fixture.Accounts[0].balance).equal(24.25)
-    })
+  it('first account should contain balance 24.25', function() {
+    should(fixture.Accounts[0].balance).equal(24.25)
+  })
 
-    it('second account should contain balance 0.00', function() {
-        should(fixture.Accounts[1].balance).equal(0.00)
-    })
+  it('second account should contain balance 0.00', function() {
+    should(fixture.Accounts[1].balance).equal(0.00)
+  })
 })
--- test/teardown.js
+++ test/teardown.js
@@ -2,29 +2,29 @@ var should = require('should')
 var teardown = require('../src/teardown.js')
 
 describe('teardown', function() {
-    var testString = "Lorem Ipsum dolor sit amet"
+  var testString = "Lorem Ipsum dolor sit amet"
 
-    afterEach(function() {
-      testString = "Lorem Ipsum dolor sit amet"
-    })
+  afterEach(function() {
+    testString = "Lorem Ipsum dolor sit amet"
+  })
 
-    it('should return lowercase string', function() {
-        const result = teardown(testString, "lower")
-        should(result).be.exactly("lorem ipsum dolor sit amet").and.be.a.String()
-    })
+  it('should return lowercase string', function() {
+      const result = teardown(testString, "lower")
+      should(result).be.exactly("lorem ipsum dolor sit amet").and.be.a.String()
+  })
 
-    it('should return uppercase string', function() {
-    	const result = teardown(testString, "upper")
-        should(result).be.exactly("LOREM IPSUM DOLOR SIT AMET").and.be.a.String()
-    })
+  it('should return uppercase string', function() {
+  	const result = teardown(testString, "upper")
+      should(result).be.exactly("LOREM IPSUM DOLOR SIT AMET").and.be.a.String()
+  })
 
-    it('should return capitalized string', function() {
-        const result = teardown(testString, "capitalized")
-        should(result).be.exactly("Lorem ipsum dolor sit amet").and.be.a.String()
-     })
+  it('should return capitalized string', function() {
+      const result = teardown(testString, "capitalized")
+      should(result).be.exactly("Lorem ipsum dolor sit amet").and.be.a.String()
+   })
 
-    it('should return reversed string', function() {
-    	const result = teardown(testString, "reversed")
-        should(result).be.exactly("tema tis rolod muspI meroL").and.be.a.String()
-    })
+  it('should return reversed string', function() {
+  	const result = teardown(testString, "reversed")
+      should(result).be.exactly("tema tis rolod muspI meroL").and.be.a.String()
+  })
 })
